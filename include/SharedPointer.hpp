aca0d980fd30a26892b867b52ac1bfb15f406e0e
Daniil Ryb <pochtaworld7@gmail.com>
2020-12-22 16:07:36 +0300

    added lab_03 files

--- include/SharedPointer.hpp
+++ include/SharedPointer.hpp
@@ -1,14 +1,16 @@
 // Copyright 2020 Daniil Rybakov <pochtaworld7@gmail.com>
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
-#endif // INCLUDE_HEADER_HPP_
+#ifndef INCLUDE_SHAREDPOINTER_HPP_
+#define INCLUDE_SHAREDPOINTER_HPP_
+#endif  // INCLUDE_SHAREDPOINTER_HPP_
 #include <atomic>
 #include <memory>
+#include <type_traits>
 #include <utility>
 template <typename T>
 class SharedPointer {
   T* PointerOnObject;
-  std::atomic_size_t *CountOfPointer;
+  std::atomic_uint* CountOfPointer;
+
  public:
   SharedPointer();
   SharedPointer(T* ptr);
@@ -26,88 +28,90 @@ class SharedPointer {
   void swap(SharedPointer& r);
   auto use_count() const -> size_t;
 };
-template <typename T>
+template <class T>
 SharedPointer<T>::SharedPointer() {
   PointerOnObject = nullptr;
   CountOfPointer = nullptr;
 }
-template <typename T>
-SharedPointer<T>::SharedPointer(T* ptr) {
-  ptr = PointerOnObject;
-  CountOfPointer = new std::atomic_size_t (1); // to stay is one
-}
-template <typename T>
-SharedPointer<T>::SharedPointer(const SharedPointer &r) {
-  PointerOnObject = r.PointerOnObject;
-  CountOfPointer = nullptr;
-  CountOfPointer = r.CountOfObject;
-  CountOfPointer = new std::atomic_size_t(1);  // to stay is one
-}
-template <typename T>
-SharedPointer<T>::SharedPointer(SharedPointer &&r) {}
 template <class T>
-SharedPointer<T>::~SharedPointer()  {
+SharedPointer<T>::SharedPointer(T* ptr) {
+  PointerOnObject = ptr;
+  CountOfPointer = new std::atomic_uint(1);
+}
+template <class T>
+SharedPointer<T>::SharedPointer(const SharedPointer& r) {
+  CountOfPointer = nullptr;
+  PointerOnObject = r.PointerOnObject;
+  CountOfPointer = r.CountOfPointer;
+  ++(*CountOfPointer);
+}
+template <class T>
+SharedPointer<T>::SharedPointer(SharedPointer&& r) {
+  PointerOnObject = std::move(r.PointerOnObject);
+  CountOfPointer = std::move(r.CountOfPointer);
+}
+template <class T>
+SharedPointer<T>::~SharedPointer() {
   if ((*CountOfPointer) == 0) {
     delete PointerOnObject;
     delete CountOfPointer;
   }
-  else return;
 }
-template <typename T>
-auto SharedPointer<T>::operator=(const SharedPointer &r) -> SharedPointer & {
+template <class T>
+auto SharedPointer<T>::operator=(const SharedPointer& r) -> SharedPointer& {
   if (this == &r) {
     return *this;
   }
-  this->~SharedPtr();
   PointerOnObject = r.PointerOnObject;
   CountOfPointer = r.CountOfObject;
-  (*CountOfPointer)++;
+  ++(*CountOfPointer);
   return *this;
 }
-template <typename T>
-auto SharedPointer<T>::operator=(SharedPointer &&r)  -> SharedPointer & {
+template <class T>
+auto SharedPointer<T>::operator=(SharedPointer&& r) -> SharedPointer& {
   if (this == &r) {
     return *this;
   }
-  this->~SharedPtr();
   PointerOnObject = r.PointerOnObject;
-  CountOfPointer = r.CountOfObject;
+  CountOfPointer = r.CountOfPointer;
   r.PointerOnObject = nullptr;
-  r.CountOfObject = nullptr;
+  r.CountOfPointer = nullptr;
   return *this;
 }
-template <typename T>
+template <class T>
 SharedPointer<T>::operator bool() const {
   if (CountOfPointer != nullptr) {
     return true;
-  } else { return false; }
+  } else {
+    return false;
   }
-template <typename T>
-auto SharedPointer<T>::operator*() const -> T & {
+}
+template <class T>
+auto SharedPointer<T>::operator*() const -> T& {
   return *PointerOnObject;
 }
-template <typename T>
-auto SharedPointer<T>::operator->() const -> T * {
+template <class T>
+auto SharedPointer<T>::operator->() const -> T* {
   return PointerOnObject;
 }
-template <typename T>
-auto SharedPointer<T>::get() -> T * {
-  return CountOfPointer;
+template <class T>
+auto SharedPointer<T>::get() -> T* {
+  return PointerOnObject;
 }
-template <typename T>
+template <class T>
 void SharedPointer<T>::reset() {
   *this = std::move(SharedPointer());
 }
-template <typename T>
-void SharedPointer<T>::reset(T *ptr) {
+template <class T>
+void SharedPointer<T>::reset(T* ptr) {
   *this = std::move(SharedPointer<T>(ptr));
 }
-template <typename T>
-void SharedPointer<T>::swap(SharedPointer &r) {
+template <class T>
+void SharedPointer<T>::swap(SharedPointer& r) {
   std::swap(PointerOnObject, r.PointerOnObject);
   std::swap(CountOfPointer, r.CountOfPointer);
 }
-template <typename T>
+template <class T>
 auto SharedPointer<T>::use_count() const -> size_t {
   return static_cast<size_t>(*CountOfPointer);
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,43 @@
 // Copyright 2020 Daniil Rybakov <pochtaworld7@gmail.com>
 #include <gtest/gtest.h>
+
 #include "SharedPointer.hpp"
-SharedPointer<int> sp1;
-TEST(CheckContructor, Test1) {
-   EXPECT_EQ(sp1.use_count(), 0);
-   EXPECT_EQ((sp1.operator bool()), 0);
-   EXPECT_EQ(sp1.use_count(), 0);
+TEST(SharedPointer, ContructorDefaulTest) {
+  SharedPointer<int> SP1 = new int{0};
+  SharedPointer<int> SP2 = SP1;
+  ASSERT_EQ(SP1.operator bool(), true);
+  ASSERT_EQ(SP2.operator bool(), true);
+  ASSERT_EQ(SP1.get(), SP2.get());
+  ASSERT_EQ(SP1.use_count(), 2);
+}
+TEST(SharedPointer, Operator) {
+  int b = 50;
+  SharedPointer<int> SH_TEST(&b);
+  ASSERT_EQ(SH_TEST.use_count(), 1);
+  ASSERT_EQ(*SH_TEST, b);
+}
+TEST(SharedPointer, RvalueOperatorTest) {
+  int a = 10;
+  SharedPointer<int> SP1 = &a;
+  SharedPointer<int> SP2 = std::move(SP1);
+  ASSERT_EQ(SP1.get(), SP1.get());
+  ASSERT_EQ(SP2.use_count(), 1);
+}
+TEST(SharedPointer, SwapTest) {
+  int a = 10;
+  int b = 20;
+  SharedPointer<int> SP1(&a);
+  SharedPointer<int> SP2(&b);
+  SP2.swap(SP1);
+  ASSERT_EQ(*SP2, 10);
+  ASSERT_EQ(*SP1, 20);
+}
+TEST(SharedPointer, IsMoveConstructible) {
+  SharedPointer<int> test_sp1 = new int{10};
+  ASSERT_EQ(std::is_move_constructible<int&>::value, true);
+}
+
+TEST(SharedPointer, IsMoveAssignable) {
+  SharedPointer<int> test_sp2 = new int{10};
+  ASSERT_EQ(std::is_move_assignable<int&>::value, true);
 }
